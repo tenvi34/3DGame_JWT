38f7d2a30b208462fbdd9c4c4b8fe29798beb0df
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2025-01-06 14:04:15 +0900

    총알 구현 완료

---
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace Weapon
+{
+    public class Bullet : MonoBehaviour
+    {
+        public int damage;
+        public int destroyDelay = 3;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Environment"))
+            {
+                Destroy(gameObject); // 즉시 삭제
+                // Destroy(gameObject, destroyDelay); // 딜레이 후 삭제
+            }
+        }
+    }
+}
