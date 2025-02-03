9d4298af851b86a2d23cd5a6497a27612a22f193
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2025-02-03 11:14:15 +0900

    플레이 씬에 적용했을 때 생긴 오류 수정

---
+++ Assets/Scripts/Enemy/EnemySpawnAndPatrolManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Enemy
+{
+    public class EnemySpawnAndPatrolManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class EnemyTypeConfig
+        {
+            public GameObject enemyPrefab;
+            [Range(1, 10)] public int spawnCount;
+        }
+
+        [System.Serializable]
+        public class SpawnPointConfig
+        {
+            public Transform spawnPoint;
+            public Transform[] localPatrolPoints;
+            public EnemyTypeConfig[] enemyTypes;
+        }
+
+        [SerializeField] private SpawnPointConfig[] spawnPointConfigs;
+
+        private void Start()
+        {
+            StartCoroutine(EnemySpawnRoutine());
+        }
+
+        private IEnumerator EnemySpawnRoutine()
+        {
+            while (true)
+            {
+                foreach (var config in spawnPointConfigs)
+                {
+                    
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,14 +10,14 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     private ShooterController _shooterController;
-
-    [Header("Enemy Spawn")] [SerializeField]
-    private GameObject enemyPrefab;
-
-    [SerializeField] private Transform[] spawnPoints;
-    [SerializeField] private int[] maxEnemiesPerSpawnPoint; // 각 스폰 지점별 최대 몬스터 수
-
-    private int[] currentEnemiesAtSpawnPoint; // 현재 각 스폰 지점의 몬스터 수 추적
+    
+    // [Header("Enemy Spawn")] [SerializeField]
+    // private GameObject enemyPrefab;
+    //
+    // [SerializeField] private Transform[] spawnPoints;
+    // [SerializeField] private int[] maxEnemiesPerSpawnPoint; // 각 스폰 지점별 최대 몬스터 수
+    //
+    // private int[] currentEnemiesAtSpawnPoint; // 현재 각 스폰 지점의 몬스터 수 추적
 
     [Header("Bullet")] [SerializeField] private Text bulletText;
 
@@ -27,9 +27,9 @@ public class GameManager : MonoBehaviour
         _shooterController = FindObjectOfType<ShooterController>();
 
         // 현재 몬스터 수 배열 초기화
-        currentEnemiesAtSpawnPoint = new int[spawnPoints.Length];
+        // currentEnemiesAtSpawnPoint = new int[spawnPoints.Length];
 
-        StartCoroutine(EnemySpawn());
+        // StartCoroutine(EnemySpawn());
     }
 
     void Update()
@@ -37,47 +37,53 @@ public class GameManager : MonoBehaviour
         ShowBulletCount();
     }
 
+    /// <summary>
+    /// 총알 관리
+    /// </summary>
     void ShowBulletCount()
     {
         bulletText.text = _shooterController.currentBullet + " / " + _shooterController.maxBullet;
     }
 
-    IEnumerator EnemySpawn()
-    {
-        // 소환 가능한 스폰 포인트 찾기
-        List<int> availableSpawnPoints = new List<int>();
-        for (int i = 0; i < spawnPoints.Length; i++)
-        {
-            if (currentEnemiesAtSpawnPoint[i] < maxEnemiesPerSpawnPoint[i])
-            {
-                availableSpawnPoints.Add(i);
-            }
-        }
-
-        // 소환 가능한 지점이 있다면
-        if (availableSpawnPoints.Count > 0)
-        {
-            int selectedSpawnIndex = availableSpawnPoints[Random.Range(0, availableSpawnPoints.Count)];
-
-            // 몬스터 생성
-            GameObject spawnedEnemy = Instantiate(
-                enemyPrefab,
-                spawnPoints[selectedSpawnIndex].position,
-                Quaternion.identity
-            );
-
-            // 몬스터 수 증가
-            currentEnemiesAtSpawnPoint[selectedSpawnIndex]++;
-
-            // 몬스터 파괴 시 카운트 감소
-            // spawnedEnemy.GetComponent<Enemy.EnemyZombie>().OnEnemyDeath += DecreaseEnemyCount;
-        }
-
-        yield return new WaitForSeconds(2f);
-
-        // 2초마다 적 생성 시도
-        StartCoroutine(EnemySpawn());
-    }
+    /// <summary>
+    /// 몬스터 소환 관리
+    /// </summary>
+    // IEnumerator EnemySpawn()
+    // {
+    //     // 소환 가능한 스폰 포인트 찾기
+    //     List<int> availableSpawnPoints = new List<int>();
+    //     for (int i = 0; i < spawnPoints.Length; i++)
+    //     {
+    //         if (currentEnemiesAtSpawnPoint[i] < maxEnemiesPerSpawnPoint[i])
+    //         {
+    //             availableSpawnPoints.Add(i);
+    //         }
+    //     }
+    //
+    //     // 소환 가능한 지점이 있다면
+    //     if (availableSpawnPoints.Count > 0)
+    //     {
+    //         int selectedSpawnIndex = availableSpawnPoints[Random.Range(0, availableSpawnPoints.Count)];
+    //
+    //         // 몬스터 생성
+    //         GameObject spawnedEnemy = Instantiate(
+    //             enemyPrefab,
+    //             spawnPoints[selectedSpawnIndex].position,
+    //             Quaternion.identity
+    //         );
+    //
+    //         // 몬스터 수 증가
+    //         currentEnemiesAtSpawnPoint[selectedSpawnIndex]++;
+    //
+    //         // 몬스터 파괴 시 카운트 감소
+    //         // spawnedEnemy.GetComponent<Enemy.EnemyZombie>().OnEnemyDeath += DecreaseEnemyCount;
+    //     }
+    //
+    //     yield return new WaitForSeconds(2f);
+    //
+    //     // 2초마다 적 생성 시도
+    //     StartCoroutine(EnemySpawn());
+    // }
 
     // 몬스터 카운트 감소 메서드
     // void DecreaseEnemyCount(int spawnPointIndex)
