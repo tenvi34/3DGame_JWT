fe3d7a7d223ad6d75126f42d1ce2c50539dc5d3a
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2025-02-14 00:20:37 +0900

    플레이어 체력 스크립트 완성 및 발소리 사운드 추가

--- Assets/Scripts/Enemy/EnemyZombie.cs
+++ Assets/Scripts/Enemy/EnemyZombie.cs
@@ -9,6 +9,11 @@ namespace Enemy
         private EnemyController _enemyController;
         private Animator _animator;
         private CapsuleCollider _enemyCollider;
+        
+        private AudioSource _enemyAudioPlayer; // 오디오 소스 컴포넌트
+        public AudioClip deathSound; // 사망시 재생할 소리
+        public AudioClip hitSound; // 피격시 재생할 소리
+        public ParticleSystem hitEffect; // 피격시 재생할 파티클 효과
 
         private static readonly int Dead = Animator.StringToHash("Dead");
         
@@ -17,6 +22,7 @@ namespace Enemy
             _enemyController = GetComponent<EnemyController>();
             _animator = GetComponent<Animator>();
             _enemyCollider = GetComponent<CapsuleCollider>();
+            _enemyAudioPlayer = GetComponent<AudioSource>();
 
             HPBar.gameObject.SetActive(false);
         }
@@ -28,24 +34,39 @@ namespace Enemy
 
         public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
         {
-            base.OnDamage(damage, hitPoint, hitNormal);
-            
+            // 피격되면 체력바 활성화
             HPBar.gameObject.SetActive(true);
             
             if (!dead)
             {
                 _enemyController.LastKnownPlayerPosition = _enemyController.Player.transform.position;
                 _enemyController.StartChasing();
+                
+                // 공격받은 지점과 방향으로 파티클 효과 재생
+                hitEffect.transform.position = hitPoint;
+                hitEffect.transform.rotation = Quaternion.LookRotation(hitNormal);
+                hitEffect.Play();
+                
+                // 피격 효과음 재생
+                _enemyAudioPlayer.PlayOneShot(hitSound);
             }
+            
+            // LivingEntity의 OnDamage()를 실행하여 데미지 적용
+            base.OnDamage(damage, hitPoint, hitNormal);
         }
 
         public override void Die()
         {
             base.Die();
-            
-            _enemyController.NavMeshAgent.speed = 0;
-            _animator.SetTrigger(Dead);
+
+            // AI 추적을 중지하고 내비메시 컴포넌트, 콜라이더 비활성화
+            _enemyController.NavMeshAgent.isStopped = true;
+            _enemyController.NavMeshAgent.enabled = false;
             _enemyCollider.enabled = false;
+            
+            // 죽음 애니메이션 및 효과음 재생
+            _animator.SetTrigger(Dead);
+            _enemyAudioPlayer.PlayOneShot(deathSound);
 
             Destroy(gameObject, 3f);
         }
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Player
+{
+    public class PlayerHealth : LivingEntity
+    {
+        [Header("UI")]
+        [SerializeField] private Slider healthSlider; // 체력을 표시할 UI 슬라이더
+        
+        [Header("Audio")]
+        [SerializeField] private AudioClip deathClip; // 사망 소리
+        [SerializeField] private AudioClip hitClip; // 피격 소리
+        [SerializeField] private AudioClip healClip; // 회복 소리
+        
+        private AudioSource _playerAudio; // 오디오 소스 컴포넌트
+        private Animator _animator;
+        private ShooterController _shooterController;
+
+        private void Awake()
+        {
+            _playerAudio = GetComponent<AudioSource>();
+            _animator = GetComponent<Animator>();
+            _shooterController = GetComponent<ShooterController>();
+        }
+
+        // LivingEntity의 OnEnable() 실행 (상태 초기화)
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            
+            // 체력바 초기화
+            healthSlider.gameObject.SetActive(true);
+            healthSlider.maxValue = startingHealth;
+            healthSlider.value = health;
+        }
+
+        // 데미지 처리
+        public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+        {
+            // 죽지 않았을 경우에만 효과음 재생
+            if (!dead)
+            {
+                _playerAudio.PlayOneShot(hitClip);
+            }
+            // LivingEntity의 OnDamage() 실행(데미지 적용)
+            base.OnDamage(damage, hitPoint, hitNormal);
+            
+            // 체력바 갱신
+            healthSlider.value = health;
+        }
+        
+        // 체력 회복
+        public override void RestoreHealth(float newHealth)
+        {
+            // LivingEntity의 RestoreHealth() 실행 (체력 증가)
+            base.RestoreHealth(newHealth);
+            
+            // 체력바 갱신
+            healthSlider.value = health;
+            _playerAudio.PlayOneShot(healClip);
+        }
+
+        // 사망 처리
+        public override void Die()
+        {
+            // LivingEntity의 Die() 실행(사망 적용)
+            base.Die();
+            
+            // 체력바 비활성화
+            healthSlider.gameObject.SetActive(false);
+            
+            // 사망 애니메이션 재생 및 효과음 재생
+            _playerAudio.PlayOneShot(deathClip);
+            _animator.SetTrigger("Die");
+            
+            // 플레이어 조작 비활성화
+            _shooterController.enabled = false;
+            
+            // 게임 오버 처리
+            GameManager.Instance.EndGame();
+        }
+    }
+}
